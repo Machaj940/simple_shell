24b221e7525d19b6cc3296aa92d6083a6d954ca2
John Macharia <machariajohn23@gmail.com>
2022-01-17 05:37:28 -0800

    A beautiful code that passes the Betty checks

---
+++ betty_checks.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of a string
+ * @s: a string
+ * Return: c
+ */
+int _strlen(char *s)
+{
+	int c = 0;
+
+	while (s[c] != '\0')
+		c++;
+	return (c);
+}
